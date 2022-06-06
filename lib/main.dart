f4c3f8e4b2a3b2c1b6ce319ce498590093023c86
SimonOmalley <91579437+SimonOmalley@users.noreply.github.com>
2022-06-06 11:26:19 +1000

    Basic App development

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-      title: 'Tas Waste App',
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello World',
-            textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    title: "Tas Waste App",
+    home: MaterialApp(
+        title: 'Tas Waste App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Dashboard'.toUpperCase()),
+            backgroundColor: Colors.amber,
           ),
-        ),
-      )));
+          body: const Center(
+            child: Text.rich(
+              TextSpan(
+                text: "My",
+                children: [
+                  TextSpan(
+                      text: "Flutter",
+                      style: TextStyle(
+                          fontSize: 50.0, fontWeight: FontWeight.bold)),
+                  TextSpan(
+                      text: "App",
+                      style: TextStyle(fontSize: 30.0, color: Colors.red)),
+                ],
+              ),
+            ),
+          ),
+        )),
+  ));
 }
