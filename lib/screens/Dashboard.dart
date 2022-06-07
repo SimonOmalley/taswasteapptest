421d4695293a53e553664f93d775d1d3a1d21638
SimonOmalley <91579437+SimonOmalley@users.noreply.github.com>
2022-06-07 14:23:19 +1000

    Font

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -27,7 +27,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lime,
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text(
@@ -57,6 +56,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
